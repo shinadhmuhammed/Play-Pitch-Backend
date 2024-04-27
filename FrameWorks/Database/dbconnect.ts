a27bbcd31a0fe160af3e751deb4f5ca7eab78a10
muhammed shinadh <muhammedshinad2@gmail.com>
2024-04-27 14:37:32 +0530

    formated code

--- FrameWorks/Database/dbconnect.ts
+++ FrameWorks/Database/dbconnect.ts
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
-import dotenv from 'dotenv'
-dotenv.config()
+import dotenv from "dotenv";
+dotenv.config();
 
-const mongoURI: string = process.env.MONGODB_URI || ''; 
+const mongoURI: string = process.env.MONGODB_URI || "";
 
 mongoose.connect(mongoURI);
 
