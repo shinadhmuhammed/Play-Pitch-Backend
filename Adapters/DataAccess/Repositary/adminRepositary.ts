66283baff2e7180a5fe36ca6ef85119b9733b916
muhammed shinadh <muhammedshinad2@gmail.com>
2024-04-27 14:46:13 +0530

    impliment findadmin,getusers,blockunblock and booking dashboard

--- Adapters/DataAccess/Repositary/adminRepositary.ts
+++ Adapters/DataAccess/Repositary/adminRepositary.ts
@@ -2,55 +2,96 @@ import { ObjectId } from "mongodb";
 import Admin from "../Models/adminModel";
 import User from "../Models/UserModel";
 import Turf from "../Models/turfModel";
+import TurfBooking from "../Models/bookingModel";
+import moment from "moment";
+
+const findAdmin = async (email: string) => {
+  try {
+    const adminData = await Admin.findOne({ email: email });
+    return adminData;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getusers = async () => {
+  try {
+    const users = await User.find();
+    return users;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const blockunblock = async (email: string, isBlocked: boolean) => {
+  try {
+    return await User.updateOne(
+      { email: email },
+      { $set: { isBlocked: !isBlocked } }
+    );
+  } catch (error) {
+    console.log("error in blocking the user", error);
+  }
+};
+
+const getTurf = async () => {
+  try {
+    const turf = await Turf.find();
+    return turf;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getVenueId = async (venueId: string) => {
+  try {
+    console.log(venueId, "venueIddd");
+    const venue = await Turf.findOne({ _id: venueId });
+    return venue;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const bookingdashboardRepositary =async()=> {
+  const getTotalBookings=await TurfBooking.countDocuments();
+    return getTotalBookings
+  }
 
 
-const findAdmin=async(email:string)=>{
-    try {
-        const adminData=await Admin.findOne({email:email})
-            return adminData
-    } catch (error) {
-        console.log(error)
-    }
-}
+
+const UserDashboardRepository =async()=> {
+  const getTotalUsers= await User.countDocuments();
+    return getTotalUsers
+  }
 
 
-const getusers=async()=>{
-    try {
-        const users=await User.find()
-        return users
-    } catch (error) {
-        console.log(error)
-    }
-}
+  const getTodayBookings=async ()=> {
+    const todayStart = moment().startOf('day');
+    const todayEnd = moment().endOf('day');
+    return await TurfBooking.countDocuments({
+      date: { $gte: todayStart, $lte: todayEnd }
+    });
+  }
 
-const blockunblock=async(email:string,isBlocked:boolean)=>{
-        try {
-            return await User.updateOne({email:email},{$set:{isBlocked:!isBlocked}})
-        } catch (error) {
-                console.log('error in blocking the user',error)
-        }
-}
-
-const getTurf=async()=>{
-    try {
-        const turf=await Turf.find()
-        return turf
-    } catch (error) {
-        console.log(error)
-    }
-}
-
-const getVenueId=async(venueId:string)=>{
-    try {
-        console.log(venueId,'venueIddd')
-        const venue = await Turf.findOne({ _id: venueId });
-        console.log(venue,'hai nageeeesa')
-        return venue
-    } catch (error) {
-        console.log(error)
-    }
-}
+  const  getMonthlyBookings=async()=> {
+    const firstDayOfMonth = moment().startOf('month');
+    const lastDayOfMonth = moment().endOf('month');
+    return await TurfBooking.countDocuments({
+      date: { $gte: firstDayOfMonth, $lte: lastDayOfMonth }
+    });
+  }
 
 
 
-export default {findAdmin,getusers,blockunblock,getTurf,getVenueId}
+export default {
+  findAdmin,
+  getusers,
+  blockunblock,
+  getTurf,
+  getVenueId,
+  UserDashboardRepository,
+  bookingdashboardRepositary,
+  getTodayBookings,
+  getMonthlyBookings
+};
