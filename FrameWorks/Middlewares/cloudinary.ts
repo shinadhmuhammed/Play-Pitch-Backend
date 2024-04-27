a26a4ab8c587b0b0e7c31e50c6c432919f482dfc
muhammed shinadh <muhammedshinad2@gmail.com>
2024-04-27 14:38:55 +0530

    clean the code in cloudinary

--- FrameWorks/Middlewares/cloudinary.ts
+++ FrameWorks/Middlewares/cloudinary.ts
@@ -1,14 +1,12 @@
-import dotenv from 'dotenv';
-import { v2 as cloudinary, UploadApiResponse } from 'cloudinary'; 
-
+import dotenv from "dotenv";
+import { v2 as cloudinary, UploadApiResponse } from "cloudinary";
 
 dotenv.config();
 
-
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 export const cloudinaryInstance = cloudinary;
