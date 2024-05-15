37c28b1a6ef025bbac9e505fd2ce1043f7816fc3
muhammed shinadh <muhammedshinad2@gmail.com>
2024-05-15 13:25:25 +0530

    Update CORS policy to allow requests from hosting environment

--- server.ts
+++ server.ts
@@ -13,7 +13,8 @@ const app = express();
 const server = http.createServer(app);
 const io = new SocketIOServer(server, {
   cors: {
-    origin: "http://localhost:5173/",
+    origin: "https://playpitch.vercel.app",
+
   },
 });
 
